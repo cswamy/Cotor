d945c82d19547898de38296e627b45997e81a356
cswamy <101974014+cswamy@users.noreply.github.com>
2024-02-28 23:06:04 +0000

    search text fields and fonts

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -5,28 +5,43 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { AuthProvider } from './context/AuthProvider';
 import ProtectedRoute from './context/ProtectedRoute';
 
+// Import mui components
+import {
+  ThemeProvider,
+  createTheme,
+} from '@mui/material';
+
 // Import custom components
 import HomePage from './components/HomePage/HomePage';
 import SearchIssue from './components/SearchIssue/SearchIssue';
 import NotFoundPage from './components/NotFoundPage/NotFoundPage';
 
+// Create theme
+const theme = createTheme ({
+  typography: {
+    fontFamily: "'DM Sans', sans-serif",
+  },
+});
+
 const App = () => {
   return (
-    <BrowserRouter>
-      <AuthProvider>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/search" 
-          element={
-            <ProtectedRoute>
-              <SearchIssue />
-            </ProtectedRoute>
-            } 
-          />
-          <Route path="*" element={<NotFoundPage />} />
-        </Routes>
-      </AuthProvider>
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <AuthProvider>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/search" 
+            element={
+              <ProtectedRoute>
+                <SearchIssue />
+              </ProtectedRoute>
+              } 
+            />
+            <Route path="*" element={<NotFoundPage />} />
+          </Routes>
+        </AuthProvider>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 };
 
--- webapp/src/components/SearchIssue/SearchIssue.jsx
+++ webapp/src/components/SearchIssue/SearchIssue.jsx
@@ -4,6 +4,7 @@ import { React } from 'react';
 import {
     Box,
     Typography,
+    Grid,
     TextField,
     useTheme,
     useMediaQuery,
@@ -35,6 +36,89 @@ const SearchIssue = () => {
                         Cotor
                     </Typography>
                 </Box>
+                <Grid container justify="center">
+                    <Grid item xs={12}>
+                        <Box 
+                        width={isSmallScreen ? '70%' : '40%'} 
+                        margin="auto"
+                        sx={{mt: 3}}
+                        >   
+                            <Typography 
+                            variant={isSmallScreen ? 'body1' : 'h6'} 
+                            align="center"
+                            sx={{mb: 1}}>
+                            Paste link to a public GitHub repository
+                            </Typography>
+                            <TextField 
+                            fullWidth
+                            placeholder="https://github.com/gradio-app/gradio"
+                            inputProps={{
+                                style: {
+                                    fontSize: isSmallScreen ? "0.8rem" : "1rem", 
+                                    textAlign: "center" 
+                                }
+                            }}
+                            sx={{
+                                '& .MuiOutlinedInput-root': {
+                                    '& fieldset': {
+                                        borderColor: 'grey',
+                                        borderRadius: '25px',
+                                    },
+                                    '&:hover fieldset': {
+                                        borderColor: 'black',
+                                        border: '1px solid',
+                                    },
+                                    '&.Mui-focused fieldset': {
+                                        borderColor: 'black',
+                                        border: '1px solid',
+                                    },
+                                },
+                            }}
+                            />
+                        </Box>
+                    </Grid>
+
+                    <Grid item xs={12}>
+                        <Box 
+                        width={isSmallScreen ? '40%' : '20%'} 
+                        margin="auto"
+                        sx={{mt: 4}}
+                        >   
+                            <Typography 
+                            variant={isSmallScreen ? 'body1' : 'h6'} 
+                            align="center"
+                            sx={{mb: 1}}>
+                            Enter an issue number
+                            </Typography>
+                            <TextField 
+                            fullWidth
+                            placeholder="6973"
+                            inputProps={{
+                                style: {
+                                    fontSize: isSmallScreen ? "0.8rem" : "1rem", 
+                                    textAlign: "center" 
+                                }
+                            }}
+                            sx={{
+                                '& .MuiOutlinedInput-root': {
+                                    '& fieldset': {
+                                        borderColor: 'grey',
+                                        borderRadius: '25px',
+                                    },
+                                    '&:hover fieldset': {
+                                        borderColor: 'black',
+                                        border: '1px solid',
+                                    },
+                                    '&.Mui-focused fieldset': {
+                                        borderColor: 'black',
+                                        border: '1px solid',
+                                    },
+                                },
+                            }}
+                            />
+                        </Box>
+                    </Grid>
+                </Grid>
             </Box>
         </Box>
     );
