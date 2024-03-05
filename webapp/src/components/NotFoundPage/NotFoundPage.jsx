cc0bea31d804dfd61705ab6c156cf7e53567a829
cswamy <101974014+cswamy@users.noreply.github.com>
2024-03-05 18:28:02 +0000

    not found page

--- webapp/src/components/NotFoundPage/NotFoundPage.jsx
+++ webapp/src/components/NotFoundPage/NotFoundPage.jsx
@@ -3,12 +3,56 @@ import { React } from 'react';
 // Import mui components
 import {
     Box,
+    Button,
+    Typography,
+    useTheme,
+    useMediaQuery,
 } from '@mui/material';
 
+// Import networking
+import { useNavigate } from 'react-router-dom';
+
 const NotFoundPage = () => {
+
+    const navigate = useNavigate();
+    const theme = useTheme();
+    const isSmallScreen = useMediaQuery(theme.breakpoints.down('sm'));
+
     return (
-        <Box>
-            <h1>404: Page not found</h1>
+        <Box
+        sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center',
+            mt: 10,
+        }}>
+            <Typography variant="h3" align="center">
+                Nooooooo!
+            </Typography>
+            <Typography variant="h6" align="center">
+                {`How have we found ourselves here :(`}
+            </Typography>
+            <Typography variant="subtitle1" align="center">
+                Please click on search and try again
+            </Typography>
+            <Button
+            sx={{ 
+                mt: 2,
+                backgroundColor: '#dadada',
+                color: 'black',
+                borderRadius: '25px',
+                '&:hover': {
+                    backgroundColor: 'black',
+                    color: 'white',
+                },
+                textTransform: 'none',
+                width: isSmallScreen ? '30%' : '10%',
+            }}
+            onClick={() => {navigate('/search', {'replace': true});}}
+            >
+                Search
+            </Button>
         </Box>
     );
 }
