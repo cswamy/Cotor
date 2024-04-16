9050b50b72db014b6c6b418ebd56bf496d6511b7
cswamy <101974014+cswamy@users.noreply.github.com>
2024-04-16 15:27:01 +0100

    updates around cosine_similarity

--- prototyping/agents/main.py
+++ prototyping/agents/main.py
@@ -1,13 +1,31 @@
 import os
 import utils
+import pandas as pd
 
 repo = '../../../gradio'
+repo_link = 'https://github.com/gradio-app/gradio'
 folders = ['gradio', 'test']
 repo_folders = [f"{repo}/{folder}" for folder in folders]
+issue = 6973
 
-# Get dataframe with repo, folder, file, raw_code
-df = utils.extract_files(repo, repo_folders)
+if not os.path.exists('embedding.csv'):
+    # Get dataframe with repo, folder, file, raw_code
+    df = utils.extract_repo_files(repo, repo_folders)
 
-# Add embeddings to the dataframe
-df = utils.add_embeddings(df)
-print(df)
+    # Add embeddings to the dataframe
+    df = utils.add_repo_embeddings(df)
+
+    # Write dataframe to csv
+    with open('embedding.csv', 'w') as f:
+        df.to_csv(f, index=False)
+
+# Read dataframe from csv
+df = pd.read_csv('embedding.csv')
+
+# Get embedding for GitHub issue title, body and comments
+issue_embed = utils.get_issue_embed(repo_link, issue)
+
+# Get candidate files based on cosine similarity
+if len(issue_embed) > 0: 
+    files = utils.get_candidate_files(df, issue_embed)
+    print(files)
